5db4a02ee4e77e95433114cfee485c65edae0efd
Zenny0919 <103423680+Zenny0919@users.noreply.github.com>
2022-09-01 22:38:39 +1000

    Zhenyu js commit.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,19 @@
 // write your code here
+arr = [];
+element = 0;
+for (let i = 1; i <= 100; i++) {
+    if (i % 3 || i % 5 !== 0) {
+        element = i;
+        if (i % 3 === 0) {
+            element = 'Fizz';
+        } else if (i % 5 === 0) {
+            element = 'Buzz'
+        } 
+        } else {
+            element = 'FizzBuzz';
+        }
+    arr.push(element);
+}
 
+console.log(arr);
 
